d1a283b584d4530002421271378a754273ca243b
Stanislav <stankhil@gmail.com>
2025-01-30 17:41:25 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            start = new Button();
+            current = new Label();
+            textBox1 = new TextBox();
+            add = new Button();
+            inputWords = new TextBox();
+            delete = new Button();
+            wordsList = new ListBox();
+            process = new ProgressBar();
+            SuspendLayout();
+            // 
+            // start
+            // 
+            start.Location = new Point(155, 212);
+            start.Name = "start";
+            start.Size = new Size(75, 23);
+            start.TabIndex = 0;
+            start.Text = "Start";
+            start.UseVisualStyleBackColor = true;
+            start.Click += start_Click;
+            // 
+            // current
+            // 
+            current.AutoSize = true;
+            current.Location = new Point(142, 275);
+            current.Name = "current";
+            current.Size = new Size(0, 15);
+            current.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 275);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(677, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // add
+            // 
+            add.Location = new Point(691, 195);
+            add.Name = "add";
+            add.Size = new Size(61, 40);
+            add.TabIndex = 4;
+            add.Text = "Add";
+            add.UseVisualStyleBackColor = true;
+            add.Click += add_Click;
+            // 
+            // inputWords
+            // 
+            inputWords.Location = new Point(558, 212);
+            inputWords.Name = "inputWords";
+            inputWords.Size = new Size(100, 23);
+            inputWords.TabIndex = 5;
+            // 
+            // delete
+            // 
+            delete.Location = new Point(691, 131);
+            delete.Name = "delete";
+            delete.Size = new Size(61, 40);
+            delete.TabIndex = 6;
+            delete.Text = "Delete";
+            delete.UseVisualStyleBackColor = true;
+            delete.Click += delete_Click;
+            // 
+            // wordsList
+            // 
+            wordsList.FormattingEnabled = true;
+            wordsList.Location = new Point(558, 101);
+            wordsList.Name = "wordsList";
+            wordsList.Size = new Size(120, 94);
+            wordsList.TabIndex = 7;
+            wordsList.DoubleClick += wordsList_DoubleClick;
+            // 
+            // process
+            // 
+            process.Location = new Point(142, 246);
+            process.Name = "process";
+            process.Size = new Size(100, 23);
+            process.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(process);
+            Controls.Add(wordsList);
+            Controls.Add(delete);
+            Controls.Add(inputWords);
+            Controls.Add(add);
+            Controls.Add(textBox1);
+            Controls.Add(current);
+            Controls.Add(start);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button start;
+        private Label current;
+        private TextBox textBox1;
+        private Button add;
+        private TextBox inputWords;
+        private Button delete;
+        private ListBox wordsList;
+        private ProgressBar process;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mime;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        List<string> words = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void start_Click(object sender, EventArgs e)
+        {
+            if(words.Count == 0)
+            {
+                MessageBox.Show("Words list is empty");
+                return;
+            }
+            string rootPath = @"C:\Users\user\";
+
+            try
+            {
+                var directoriesQueue = new Queue<string>();
+                directoriesQueue.Enqueue(rootPath);
+                List<string> allFiles = new List<string>();
+
+                MessageBox.Show("Іде підготовка директорії, дочікуйтеся старту");
+
+                while (directoriesQueue.Count > 0)
+                {
+                    string currentDirectory = directoriesQueue.Dequeue();
+                    try
+                    {
+                        allFiles.AddRange(Directory.GetFiles(currentDirectory, "*.txt"));
+
+                        var subDirectories = Directory.GetDirectories(currentDirectory);
+                        foreach (var subDirectory in subDirectories)
+                        {
+                            directoriesQueue.Enqueue(subDirectory);
+                        }
+                    }
+                    catch (UnauthorizedAccessException) { }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show($"Ошибка при обработке директории {currentDirectory}: {ex.Message}");
+                    }
+                }
+
+                process.Maximum = allFiles.Count;
+                process.Value = 0;
+
+
+                foreach (string file in allFiles)
+                {
+                    try
+                    {
+                        textBox1.Text = file;
+                        await ReadFileAsync(file);
+                        process.Value++;
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show($"Ошибка при обработке файла {file}: {ex.Message}");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}");
+            }
+
+            MessageBox.Show("Сканирование завершено.");
+        }
+
+        private async Task ReadFileAsync(string file)
+        {
+            string content = await File.ReadAllTextAsync(file);
+            string fileName = Path.GetFileName(file);
+            string newText = content;
+
+            foreach (var word in words)
+            {
+                if (content.Contains(word))
+                {
+                    MessageBox.Show($"Файл содержит слово {word}: {fileName}\n{file}");
+                    newText = newText.Replace(word, new string('*', word.Length));
+                }
+            }
+
+            if (!content.Equals(newText))
+            {
+                await CreateCopyAsync(file, newText);
+            }
+        }
+
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Слово добавлено в список");
+            if (!string.IsNullOrWhiteSpace(inputWords.Text))
+            {
+                wordsList.Items.Add(inputWords.Text);
+                words.Add(inputWords.Text);
+                inputWords.Clear();
+            }
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            if (wordsList.SelectedItem != null)
+            {
+                words.Remove(wordsList.SelectedItem.ToString());
+                wordsList.Items.Remove(wordsList.SelectedItem);
+                
+            }
+        }
+
+        private void wordsList_DoubleClick(object sender, EventArgs e)
+        {
+            if (wordsList.SelectedItem != null)
+            {
+                words.Remove(wordsList.SelectedItem.ToString());
+                wordsList.Items.Remove(wordsList.SelectedItem);
+                
+            }
+        }
+
+        private async Task CreateCopyAsync(string originalFilePath, string newContent)
+        {
+            try
+            {
+                string changedFilesDir = @"C:\Users\user\ChangedFiles";
+                if (!Directory.Exists(changedFilesDir))
+                {
+                    Directory.CreateDirectory(changedFilesDir);
+                }
+                string fileName = Path.GetFileName(originalFilePath);
+                string newFilePath = Path.Combine(changedFilesDir, fileName);
+
+                await File.WriteAllTextAsync(newFilePath, newContent);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при создании копии файла: {ex.Message}");
+            }
+        }
+
+    }
+}
